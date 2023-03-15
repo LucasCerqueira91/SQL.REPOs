30af8ca90ca0818ca4d92a50a779d98e8eaea9a7
Lucas Cerqueira Santos <lucascerqueira.art@gmail.com>
2023-03-15 20:17:32 +0000

    Update CookingGrams.sql

--- CookingGrams.sql
+++ CookingGrams.sql
@@ -1,5 +1,9 @@
 USE CookingGrams
-DROP TABLE Difficulty Recipe_Favorite
+use Vehicles
+CREATE DATABASE CookingGrams
+
+DROP TABLE DATABASE CookingGrams;
+Difficulty Recipe_Favorite
 
 CREATE TABLE Categories (
 ID INT PRIMARY KEY IDENTITY,
@@ -21,9 +25,13 @@ Name_I NVARCHAR (50));
 CREATE TABLE Ingredients_Description(
 ID INT PRIMARY KEY IDENTITY,
 Description_IDesc NVARCHAR (50) NOT NULL,
-Quantity_Gr INT NOT NULL,
 FKIngredient_ID INT NOT NULL,
-FkRecipes_Ingredient_ID INT NOT NULL);  
+FkRecipes_Ingredient_ID INT NOT NULL,
+FkIngredients_Unit_Measurement_ID INT NOT NULL);  
+
+CREATE TABLE Ingredients_Unit_Measurement(
+ID INT PRIMARY KEY IDENTITY,
+Measurement_Unit NVARCHAR (15) NOT NULL CHECK (Measurement_Unit IN('Kg','Ml','Gr','Lts')));
 
 CREATE TABLE Ratings (
 ID INT PRIMARY KEY IDENTITY,
@@ -38,9 +46,10 @@ FkUser_ID INT NOT NULL,
 FkRecipe_ID INT NOT NULL,
 FkCategory_ID INT NOT NULL);
 
-CREATE TABLE Recipes_Ingredients (
+/*CREATE TABLE Recipes_Ingredients (
 ID INT PRIMARY KEY IDENTITY,
 FkRecipe_ID INT NOT NULL);
+*/
 
 CREATE TABLE Recipes (
 ID INT PRIMARY KEY IDENTITY,
@@ -59,16 +68,16 @@ Password_U NVARCHAR(2000) NOT NULL,
 IsBlocked_U NCHAR (1) NOT NULL,
 Admin_U NCHAR(1) NOT NULL);
 
-CREATE TABLE Unit_Measurement(
-ID INT PRIMARY KEY IDENTITY,
-Name_Unit NVARCHAR (15) NOT NULL CHECK (Name_Unit IN('Kg','Ml','Gr','Lts')));
 
-Ingredients_Description,FKIngredient_ID,FkRecipes_Ingredient_ID
-ALTER TABLE Ingredients_Description ADD CONSTRAINT FkRecipes_Ingredient_ID_Ingredient_Description
-FOREIGN KEY (FkRecipes_Ingredient_ID) REFERENCES Ingredients(ID);
+
+Ingredients_Description, FKIngredient_ID, FkIngredients_Unit_Measurement_ID
+
+
+ALTER TABLE Ingredients_Description ADD CONSTRAINT FkIngredient_ID_Ingredient_Description
+FOREIGN KEY (FKIngredient_ID) REFERENCES Ingredients(ID);
 
 ALTER TABLE Ingredients_Description ADD CONSTRAINT FKUnit_Measurement_ID_Ingredient_Description
-FOREIGN KEY (FKUnit_Measurement) REFERENCES Unit_Measurement(ID);
+FOREIGN KEY (FkIngredients_Unit_Measurement_ID) REFERENCES Ingredients_Unit_Measurement(ID);
 
 ALTER TABLE Ratings ADD CONSTRAINT FkUser_ID_Ratings
 FOREIGN KEY (FkUser_ID) REFERENCES Users(ID);
@@ -88,12 +97,15 @@ FOREIGN KEY (FkCategory_ID) REFERENCES Categories(ID);
 ALTER TABLE Recipes ADD CONSTRAINT Category_ID_Recipes
 FOREIGN KEY (FkCategory_ID) REFERENCES Categories(ID);
 
-ALTER TABLE Recipes ADD Difficulty INT NOT NULL
+ALTER TABLE Recipes ADD CONSTRAINT Recipes_ID_Users
+FOREIGN KEY (FkUser_ID) REFERENCES Users(ID);
 
-ALTER TABLE Ingredients_Description ADD FKUnit_Measurement INT NOT NULL;
+ALTER TABLE Ingredients_Description DROP COLUMN FkIngredients_Description_ID INT NOT NULL
 
-ALTER TABLE Categories ADD CONSTRAINT Categories_ID_Recipes
-FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
+ALTER TABLE Recipes ADD FkIngredients_Description_ID INT NOT NULL;
+
+--ALTER TABLE Categories ADD CONSTRAINT Categories_ID_Recipes
+--FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
 
 ALTER TABLE Recipes_Ingredients ADD CONSTRAINT Recipes_ID_Recipes_Ingredients
 FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
@@ -101,7 +113,5 @@ FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
 ALTER TABLE Recipes_Ingredients ADD CONSTRAINT Ingredients_ID_Recipes_Ingredients
 FOREIGN KEY (FkIngredient_ID) REFERENCES Ingredients(ID);
 
-ALTER TABLE Recipes ADD CONSTRAINT Recipes_ID_Users
-FOREIGN KEY (FkUser_ID) REFERENCES Users(ID);
-
-  
+ALTER TABLE Recipes ADD CONSTRAINT Ingredients_Description_ID_Recipes
+FOREIGN KEY (FkIngredients_Description_ID) REFERENCES Ingredients_Description(ID);
