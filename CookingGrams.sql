801db96b2d3e9b144c8ad860fe48b5aab843bef2
Lucas Cerqueira Santos <lucascerqueira.art@gmail.com>
2023-04-10 10:24:43 -0300

    Update CookingGrams.sql

--- CookingGrams.sql
+++ CookingGrams.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE Cookingrams
+
 USE CookingGrams
 use Vehicles
 CREATE DATABASE CookingGrams
@@ -8,7 +10,7 @@ Difficulty Recipe_Favorite
 CREATE TABLE Categories (
 ID INT PRIMARY KEY IDENTITY,
 Title_C NVARCHAR (50) NOT NULL,
---FkRecipe_ID INT NOT NULL
+FkRecipe_ID INT NOT NULL
 );
 
 --CREATE TABLE Difficulty(
@@ -27,11 +29,12 @@ CREATE TABLE Ingredients_Description(
 ID INT PRIMARY KEY IDENTITY,
 Description_IDesc NVARCHAR (50) NOT NULL,
 FKIngredient_ID INT NOT NULL,
---FkRecipes_Ingredient_ID INT NOT NULL,
+FkRecipe_ID INT NOT NULL,
 FkIngredients_Unit_Measurement_ID INT NOT NULL);  
 
 CREATE TABLE Ingredients_Unit_Measurement(
 ID INT PRIMARY KEY IDENTITY,
+Quantity_ INT NOT NULL,
 Measurement_Unit NVARCHAR (15) NOT NULL CHECK (Measurement_Unit IN('Kg','Ml','Gr','Lts')));
 
 CREATE TABLE Ratings (
@@ -39,7 +42,7 @@ ID INT PRIMARY KEY IDENTITY,
 Rating_U NCHAR (1) NOT NULL,
 Comment_U NVARCHAR(100) NULL,
 FkUser_ID INT NOT NULL,
---FkRecipe_ID INT NOT NULL
+FkRecipe_ID INT NOT NULL
 );
 
 CREATE TABLE Recipes_Favorite(
@@ -59,10 +62,10 @@ Title_R NVARCHAR (100) NOT NULL,
 Description_R NVARCHAR(200) NOT NULL,
 PreparationMethod_R NVARCHAR(200) NOT NULL,
 PreparationTime NVARCHAR(50) NOT NULL,
-FkUser_ID INT NOT NULL,
-FkCategory_ID INT NOT NULL,
-FkRating_ID INT NOT NULL,
-FkIngredients_Description_ID INT NOT NULL);
+FkUser_ID INT NOT NULL);
+--FkCategory_ID INT NOT NULL,
+--FkRating_ID INT NOT NULL,
+--FkIngredients_Description_ID INT NOT NULL);
 
 CREATE TABLE Users (
 ID INT PRIMARY KEY IDENTITY,
@@ -81,11 +84,16 @@ FOREIGN KEY (FKIngredient_ID) REFERENCES Ingredients(ID);
 ALTER TABLE Ingredients_Description ADD CONSTRAINT FKUnit_Measurement_ID_Ingredient_Description
 FOREIGN KEY (FkIngredients_Unit_Measurement_ID) REFERENCES Ingredients_Unit_Measurement(ID);
 
+
+ALTER TABLE Ingredients_Description ADD CONSTRAINT FkRecipe_ID_Ingredient_Description
+FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
+
+
 ALTER TABLE Ratings ADD CONSTRAINT FkUser_ID_Ratings
 FOREIGN KEY (FkUser_ID) REFERENCES Users(ID);
 
---ALTER TABLE Ratings ADD CONSTRAINT FkRecipe_ID_Ratings
---FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
+ALTER TABLE Ratings ADD CONSTRAINT FkRecipe_ID_Ratings
+FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
 
 ALTER TABLE Recipes_Favorite ADD CONSTRAINT Recipes_Favorite_ID_Users
 FOREIGN KEY (FkUser_ID) REFERENCES Users(ID);
@@ -96,24 +104,24 @@ FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
 ALTER TABLE Recipes_Favorite ADD CONSTRAINT Recipes_Favorite_ID_Categories
 FOREIGN KEY (FkCategory_ID) REFERENCES Categories(ID);
 
-ALTER TABLE Recipes ADD CONSTRAINT Category_ID_Recipes
-FOREIGN KEY (FkCategory_ID) REFERENCES Categories(ID);
+--ALTER TABLE Recipes ADD CONSTRAINT Category_ID_Recipes
+--FOREIGN KEY (FkCategory_ID) REFERENCES Categories(ID);
 
 ALTER TABLE Recipes ADD CONSTRAINT Recipes_ID_Users
 FOREIGN KEY (FkUser_ID) REFERENCES Users(ID);
 
-ALTER TABLE Recipes ADD CONSTRAINT Recipes_ID_Ratings
-FOREIGN KEY (FkRating_ID) REFERENCES Ratings(ID);
+--ALTER TABLE Recipes ADD CONSTRAINT Recipes_ID_Ratings
+--FOREIGN KEY (FkRating_ID) REFERENCES Ratings(ID);
 
-ALTER TABLE Recipes ADD CONSTRAINT Recipes_ID_Ingredients_Description
-FOREIGN KEY (FkIngredients_Description_ID) REFERENCES Ingredients_Description(ID);
+--ALTER TABLE Recipes ADD CONSTRAINT Recipes_ID_Ingredients_Description
+--FOREIGN KEY (FkIngredients_Description_ID) REFERENCES Ingredients_Description(ID);
 
 ALTER TABLE Ingredients_Description DROP COLUMN FkIngredients_Description_ID INT NOT NULL
 
 ALTER TABLE Recipes ADD FkIngredients_Description_ID INT NOT NULL;
 
---ALTER TABLE Categories ADD CONSTRAINT Categories_ID_Recipes
---FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
+ALTER TABLE Categories ADD CONSTRAINT Categories_ID_Recipes
+FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
 
 --ALTER TABLE Recipes_Ingredients ADD CONSTRAINT Recipes_ID_Recipes_Ingredients
 --FOREIGN KEY (FkRecipe_ID) REFERENCES Recipes(ID);
@@ -121,8 +129,8 @@ ALTER TABLE Recipes ADD FkIngredients_Description_ID INT NOT NULL;
 --ALTER TABLE Recipes_Ingredients ADD CONSTRAINT Ingredients_ID_Recipes_Ingredients
 --FOREIGN KEY (FkIngredient_ID) REFERENCES Ingredients(ID);
 
-ALTER TABLE Recipes ADD CONSTRAINT Ingredients_Description_ID_Recipes
-FOREIGN KEY (FkIngredients_Description_ID) REFERENCES Ingredients_Description(ID);
+--ALTER TABLE Recipes ADD CONSTRAINT Ingredients_Description_ID_Recipes
+--FOREIGN KEY (FkIngredients_Description_ID) REFERENCES Ingredients_Description(ID);
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 --STORED PROCEDURES
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------
